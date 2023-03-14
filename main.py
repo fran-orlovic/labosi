0e5cfe7556edfbfee95f275979bfab93b2fd0ea3
Fran Orlović (forlovic) <forlovic@tvz.hr>
2023-03-14 19:26:06 +0100

    Labos 1

---
+++ main.py
@@ -0,0 +1,10 @@
+#Ova linija mora biti komentar!
+print('Dobrodošli', 3*'!')
+# Napisati sljedeće izraze u jednoj liniji koda
+ime = input('Unesite vaše ime: ')
+prezime = input('Unesite vaše prezime: ')
+jmbag = input('Unesite vaš JMBAG: ')
+# TODO zatražite od korisnika broj godina
+godine = input('Unesite vaš broj godina:')
+# TODO ime i prezime ispišite u navodnim znakovima
+print("Korisnik : ", ime, " ", prezime, "", godine, end='!')
