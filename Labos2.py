7f91bde3c8b0bdfb8048df1339739fd1d1cdf4f7
Fran Orlović (forlovic) <forlovic@tvz.hr>
2023-03-14 20:09:08 +0100

    Lab2 proba

---
+++ Labos2.py
@@ -0,0 +1,19 @@
+
+#Labos2
+from datetime import date
+
+
+kolegij = {}
+
+ispit = {}
+
+kolegij['ime'] = input("Unesite ime kolegija: ")
+kolegij['ects'] = input("Unesite broj ECTSa: ")
+
+date(int(input("Unesite godinu ispita: ")),int(input("Unesite mjesec ispita: " ),int(input("Unesite dan ispita: "))))
+
+print("Kolegij",kolegij['ime'].upper(),"nosi",kolegij['ects'],"ECTS bodova")
+ispit['datum'] = date
+
+print(ispit)
+
