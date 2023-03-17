ab8e35195eebd5abb7c1c23e64e1d734b0ca3f82
Fran <forlovic@tvz.hr>
2023-03-17 16:58:33 +0100

    Lab2 završen

--- Labos2.py
+++ Labos2.py
@@ -1,19 +1,27 @@
-
 #Labos2
 from datetime import date
 
-
 kolegij = {}
-
 ispit = {}
+student = {}
 
-kolegij['ime'] = input("Unesite ime kolegija: ")
-kolegij['ects'] = input("Unesite broj ECTSa: ")
+kolegij['naziv'] = input("Unesite ime kolegija: ")
+kolegij['ECTS'] = input("Unesite broj ECTSa: ")
 
-date(int(input("Unesite godinu ispita: ")),int(input("Unesite mjesec ispita: " ),int(input("Unesite dan ispita: "))))
+dan = int(input("Unesite dan ispita: "))
+mjesec = int(input("Unesite mjesec ispita: " ))
+godina = int(input("Unesite godinu ispita: "))
 
-print("Kolegij",kolegij['ime'].upper(),"nosi",kolegij['ects'],"ECTS bodova")
-ispit['datum'] = date
+#print("Kolegij",kolegij['naziv'].upper(),"nosi",kolegij['ECTS'],"ECTS bodova") # --> Zadatak 1
 
-print(ispit)
+ispit['datum'] = date(godina, mjesec, dan)
+#print("Datum ispita je:",ispit['datum']) --> Zadatak 2
 
+student['ime'] = input("Unesite ime studenta: ")
+student['prezime'] = input("Unesite prezime studenta:")
+student['ispit'] = ispit
+student['kolegij'] = kolegij
+# --> Zadatak 3
+print("\nStudent", student['ime'].capitalize(), student['prezime'].capitalize())
+print("je prijavio ispit iz kolegija", student['kolegij']['naziv'].upper())
+print("koji će se održati:", student['ispit']['datum'])
